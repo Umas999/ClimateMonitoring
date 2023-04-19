9f7bc867dafa807fe6730213a1c8ebcc549977ee
Umas999 <129519481+Umas999@users.noreply.github.com>
2023-04-20 00:02:43 +0200

    Delete Registrazione_dati_operatore.java

--- Registrazione_dati_operatore.java
+++
@@ -1,5 +0,0 @@
-public class Registrazione_dati_operatore{
-    public static void main(String[] args) {
-        /*creare centri di monitoraggio ed aggiungervi aree di interesse (solo operatori registrati e solo dopo login) */
-    }
-}
